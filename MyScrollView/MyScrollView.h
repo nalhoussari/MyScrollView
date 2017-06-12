7a94082f1e5e8ff2494d7b61c08c1d6ed082b152
nalhoussari <nalhoussari@hotmail.com>
2017-06-12 15:42:08 -0700

    Submitting the assignment

---
+++ MyScrollView/MyScrollView.h
@@ -0,0 +1,13 @@
+//
+//  MyScrollView.h
+//  MyScrollView
+//
+//  Created by Noor Alhoussari on 2017-06-12.
+//  Copyright © 2017 Noor Alhoussari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyScrollView : UIView
+
+@end
---
+++ MyScrollView/MyScrollView.m
@@ -0,0 +1,21 @@
+//
+//  MyScrollView.m
+//  MyScrollView
+//
+//  Created by Noor Alhoussari on 2017-06-12.
+//  Copyright © 2017 Noor Alhoussari. All rights reserved.
+//
+
+#import "MyScrollView.h"
+
+@implementation MyScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
